d4b8bd31482ea8403ca42e7495d2578f8db57c27
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-01-07 11:18:54 +0530

    get notification and markas read

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -1,30 +1,118 @@
-const { asyncHandler, sendSuccess, getPaginationParams } = require('../utils/responseHandler');
-const notificationService = require('../services/notificationService');
+const { success } = require('zod/v4');
+const { NotificationModel } = require('../models/notification.model.js');
+const { asyncHandler, sendSuccess, getPaginationParams } = require('../utils/responseHandler.js');
 
-/**
- * Get user notifications
- */
+
 const getNotifications = asyncHandler(async (req, res) => {
-  // const { page, limit } = getPaginationParams(req.query);
-  // const result = await notificationService.getUserNotifications(req.user._id, page, limit);
+  const { page, limit } = getPaginationParams(req.query);
+  const skip = (page - 1) * limit
+  const userId = req.id
 
-  // sendSuccess(res, result, 'Notifications retrieved successfully');
+  try {
+    const notification = await NotificationModel.find({
+      recipient: userId,
+    })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+  
+    const total = await NotificationModel.countDocuments({
+      recipient : userId
+    })
+
+    const unreadCount = await NotificationModel.countDocuments({
+      recipient : userId,
+      isRead : false
+    })
+  
+    return res.status(200).json({
+      success: true,
+      message: "Notifications retrieved successfully",
+      unreadCount : unreadCount,
+      notifications: notification,
+      pagination: {
+        total : total,
+        pages: Math.ceil(total / limit),
+        currentPage: page,
+        limit : total,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success : false,
+      message : "server error",
+      errors : error.message
+    })
+  }
 });
 
-/**
- * Mark notification as read
- */
 const markAsRead = asyncHandler(async (req, res) => {
-  // const notification = await notificationService.markAsRead(req.params.id, req.user._id);
-  // sendSuccess(res, notification, 'Notification marked as read');
+  const notificationId = req.params.id
+  const userId = req.id
+
+  try {
+    const notification = await NotificationModel.findOneAndUpdate(
+      {
+        _id : notificationId,
+        recipient : userId
+      },{
+        isRead : true,
+        readAt : new Date()
+      },{
+        new : true
+      }
+    )
+  
+    if(!notification){
+      return res.status(400).json({
+        success : false,
+        message : "notification not found"
+      })
+    }
+  
+    return res.status(200).json({
+      success: true,
+      message: "Notification marked as read",
+      notification : notification.isRead
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success : false,
+      message : "server error",
+      errors : error.message
+    })
+  }
 });
 
-/**
- * Mark all as read
- */
 const markAllAsRead = asyncHandler(async (req, res) => {
-  // await notificationService.markAllAsRead(req.user._id);
-  // sendSuccess(res, {}, 'All notifications marked as read');
+  const userId = req.id
+
+  try {
+    const notification = await NotificationModel.updateMany(
+      {
+        recipient : userId,
+        isRead : false
+      },{
+        isRead : true,
+        readAt : new Date()
+      }
+    )
+  
+    return res.status(200).json({
+      success: true,
+      message: "All notifications marked as read",
+      notification : notification
+    });
+    
+  } catch (error) {
+    return res.status(500).json({
+      success : false,
+      message : "server error",
+      errors : error.message
+    })
+  }
+ 
 });
 
 /**
--- src/routes/notifications.route.js
+++ src/routes/notifications.route.js
@@ -6,7 +6,6 @@ const router = express.Router();
 
 router.use(authMiddleware);
 
-// Get notifications
 router.get('/', getNotifications);
 
 // Mark as read
